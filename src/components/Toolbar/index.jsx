77d8c55db9866fd8dc86d5fdb398f1b118131dd6
Eric Bastarache <eric_bastarache@hotmail.com>
2019-05-26 03:16:35 -0400

    - WIP Refactor of components and creation of new components - Also moves NODE_PATH out of ENV in favour of jsconfig.json setting baseUrl
    Moved position of slide down

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,17 +1,91 @@
 import React from 'react';
+import {
+  connect
+} from 'react-redux';
+import Button from 'components/Button';
+import {
+  Dialog,
+  Toolbar as ToolBar,
+  AppBar,
+  Card
+} from '@material-ui/core';
+import { 
+  CREATE_SLIDE 
+} from 'types';
+import {
+  uniqid 
+} from 'lib/helpers';
 
-class Toolbar extends React.Component {
+class Toolbar extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      showPresentationModal: false,
+    }
+  }
+
+  loadPresentation = () => {
+    this.setState({showPresentationModal: !this.state.showPresentationModal});
+  }
+
+  addNewSlide = () => {
+    return this.props.addSlide();
+  }
+
+  closeModal = () => {
+    this.setState({showPresentationModal: false});
+  }
+
+  renderPresentationModal () {
+    if (this.state.showPresentationModal) {
+      return (
+        <Dialog fullScreen open={this.state.showPresentationModal} onClose={this.closeModal}>
+          <AppBar>
+            <ToolBar>
+              <Button
+                name='close-modal'
+                onClick={this.closeModal}
+              >
+                X
+              </Button>
+            </ToolBar>
+          </AppBar>
+          { this.props.presentations.map(presentation => {
+            return (
+              <Card>
+                {presentation.title}
+              </Card>
+            )
+          })}
+        </Dialog>
+      )
+    }
   }
   render () {
     return (
-      <div>
-        <button onClick={this.props.createSlide}>New Slide</button>
-        <button onClick={this.props.deleteSlide}>Delete Slide</button>
-      </div>
+      <>
+        {this.props.children({
+          loadPresentation: this.loadPresentation,
+          addSlide: this.addNewSlide
+        })}
+      {this.renderPresentationModal()}
+    </>
     )
   }
 }
 
-export default Toolbar;
+const mapStateToProps = state => {
+  return {
+    presentations: state.presentation.get('presentations')
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addSlide () {
+      return dispatch({ type: CREATE_SLIDE, payload: { id: uniqid(), title: 'Title', subtitle: 'Subtitle' } })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Toolbar);
